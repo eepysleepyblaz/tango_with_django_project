c7a289f20729edcba2b2e80c9e1cc6a3085e9f23
Sandy Emmerson <2893523e@student.gla.ac.uk>
2025-01-24 11:30:41 +0000

    Tango with Django Chapter 3

---
+++ rango/urls.py
@@ -0,0 +1,10 @@
+#Pg 37 TwD
+from django.urls import path
+from rango import views
+
+app_name = 'rango'
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('about/', views.about, name = 'about'),
+]
